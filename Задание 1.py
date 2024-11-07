2312ac372026e03ce43a0f4c9e787f5862bf8e2a
Timur <kirichenkotimur@gmail.com>
2024-11-07 20:13:39 +0300

    Задание 2, чутка перепутал, но ничего, вроде как рабочий код

--- Задание 1.py
+++ Задание 1.py
@@ -1,39 +1,59 @@
-try:
-    def f(s):
-            #Инициализация всех переменных
-        max_r=0 #максимальная доля
-        res_words=[] #список слов
-        cur_word="" #отдельное слово из строки
-        total_count=0 #общее количество букв А и Б
-        count_a=0
-        count_b=0
-        for i in s: #перебор элементов из списка
-            if i != ' ': #если символ не будет являться проблелом, то добавляем будем добавлять его в отдельную строку
-                cur_word+=i
-                if i == ('а'):
-                    count_a+=1
-                elif i == 'б':
-                    count_b+=1
-                total_count+=1 #общее воличество букв в слове
-            else:
-                if cur_word:
-                    if total_count>0:
-                        r=(count_a + count_b)/total_count #высчитываем общее количество букв а и б в слове
-                    else:
-                        r=0
+def f(s):
+        #Инициализация всех переменных
+    max_r=0 #максимальная доля
+    res_words=[] #список слов
+    cur_word="" #отдельное слово из строки
+    total_count=0 #общее количество букв А и Б
+    count_a=0
+    count_b=0
+    for i in s: #перебор элементов из списка
+        if i != ' ': #если символ не будет являться проблелом, то добавляем будем добавлять его в отдельную строку
+            cur_word+=i
+            if i == ('а'):
+                count_a+=1
+            elif i == 'б':
+                count_b+=1
+            total_count+=1 #общее воличество букв в слове
+        else:
+            if cur_word:
+                if total_count>0:
+                    r=(count_a + count_b)/total_count #высчитываем общее количество букв а и б в слове
+                else:
+                    r=0
 
-                    # сравниваем с максимальной долей в слове
-                    if r > max_r:
-                        max_r = r
-                        res_words = [cur_word]  # начинаем новый список с текущим словом
-                    elif r == max_r:
-                        res_words = [*res_words, cur_word]  # заносим это слово в список с конечным результатом
+                # сравниваем с максимальной долей в слове
+                if r > max_r:
+                    max_r = r
+                    res_words = [cur_word]  # начинаем новый список с текущим словом
+                elif r == max_r:
+                    res_words = [*res_words, cur_word]  # заносим это слово в список с конечным результатом
 
-                # сбрасываем счётчики для нового слова
-                cur_word = ""
-                total_count = 0
-                count_a = 0
-                count_b = 0
+            # сбрасываем счётчики для нового слова
+            cur_word = ""
+            total_count = 0
+            count_a = 0
+            count_b = 0
+
+    #обработка последнего слова в строке, если это не пробел
+    if cur_word:
+        if total_count>0:
+            r=(count_a + count_b)/total_count
+        else:
+            r=0
+
+        if r > max_r:
+            max_r = r
+            res_words = [cur_word]
+        elif r == max_r:
+            res_words = [*res_words, cur_word]
+
+    return res_words
+
+s="Я вообще хз, каааак должен работать это код, но он работает, поэтому нужно следовааааать золотому правилу программиста: работаааает -  не трожь"
+words_with_max_r = f(s)
+print(words_with_max_r)
+
+
 
 
 
